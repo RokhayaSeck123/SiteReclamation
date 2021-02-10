4806c728631e3d2f9ee1d9f30fe76d831cfe1c13
Rokhaya <rokhaya9818@gmail.com>
2021-02-10 11:52:24 +0000

    maxu

---
+++ src/Entity/Reclamation.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReclamationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReclamationRepository::class)
+ */
+class Reclamation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $matiere;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $professeur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $motif;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="reclamation")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?float
+    {
+        return $this->note;
+    }
+
+    public function setNote(float $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getMatiere(): ?string
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(string $matiere): self
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getProfesseur(): ?string
+    {
+        return $this->professeur;
+    }
+
+    public function setProfesseur(string $professeur): self
+    {
+        $this->professeur = $professeur;
+
+        return $this;
+    }
+
+    public function getMotif(): ?string
+    {
+        return $this->motif;
+    }
+
+    public function setMotif(string $motif): self
+    {
+        $this->motif = $motif;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation=new \DateTime();
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
